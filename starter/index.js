2a215cd4f7e555b6a0db2596be2adc7155e1e7db
Ajaya Bhogal-Todd <ajaya@Ajayas-MacBook-Pro.local>
2024-01-23 11:43:58 +0000

    Added inquirer questions and linked to render

--- starter/index.js
+++ starter/index.js
@@ -5,3 +5,134 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+let teamMembers = [];
+
+function startApp() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's ID?"
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the team manager's office number?"
+        }
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+        teamMembers.push(manager);
+        addTeamMember();
+    });
+}
+
+function addTeamMember() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberChoice',
+            message: 'Which type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'I do not want to add any more team members']
+        }
+    ]).then(answer => {
+        switch(answer.memberChoice) {
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                buildTeam();
+        }
+    });
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is the engineer's ID?"
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the engineer's email?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?"
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.github);
+        teamMembers.push(engineer);
+        addTeamMember();
+    });
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is the intern's ID?"
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the intern's email?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school does the intern attend?"
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school);
+        teamMembers.push(intern);
+        addTeamMember();
+    });
+}
+
+function buildTeam() {
+    const html = render(teamMembers);
+    fs.writeFile('./team.html', html, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Team profile generated! Check the output folder for team.html');
+    });
+}
+
+startApp();
